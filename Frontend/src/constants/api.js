081ebac426b540ef6c4f872f78a2aacf5a08bd8f
Zwubman <zwubeman@gmail.com>
2025-09-23 07:51:06 -0400

    use deployed backend link

--- Frontend/src/constants/api.js
+++ Frontend/src/constants/api.js
@@ -1,4 +1,4 @@
 // export const api = 'https://teamwork-backend-tlqq.onrender.com/api/v1'
 // export const api='http://localhost:7777/api/v1'
 
-export const api = 'http://localhost:7777/api/v1'
+export const api = 'https://software-company-i6cq.onrender.com'
